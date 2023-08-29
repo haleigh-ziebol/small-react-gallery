123dbba97fb7849d057afed4ed60a750b097b3c7
Haleigh <halziebol11@gmail.com>
2023-08-29 18:20:15 -0500

    images added, component folders and files made

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,11 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/amanita-white.jpg', description: 'Photo of a white amanita mushroom.', likes: 0 },
+    { id: 3, path: 'images/amanita-yellow.jpg', description: 'Photo of a yellow amanita mushroom.', likes: 0 },
+    { id: 4, path: 'images/white.jpg', description: 'Photo of a white mushroom with a volva.', likes: 0 },
+    { id: 5, path: 'images/old-man.jpg', description: 'Photo of an old man of the woods mushroom.', likes: 0 },
+    { id: 6, path: 'images/orangepeel.jpg', description: 'Photo of an orange peel fungus.', likes: 0 },
+    { id: 7, path: 'images/puffball.jpg', description: 'Photo of a puffball mushroom.', likes: 0 }
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,7 @@ function App() {
         </header>
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
+
       </div>
     );
 }
